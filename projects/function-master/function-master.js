4f9ca8fac96e471ca202aab7b59ef71b2c874ad7
Ubuntu <ubuntu@ip-172-31-32-165.ec2.internal>
2020-04-11 20:26:15 +0000

    function master pseudocode

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -93,81 +93,154 @@ return splitStr.join(' ');
 //////////////////////////////////////////////////////////////////////
 // Function 7 - Welcome Message //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// Should take an object with a name property and return 'Welcome <Name>!' 
+// create a variable named greeting assign with a string of "Welcome ";
+// reassign greeting concat with greeting and the object.name property value
+// return greeting
+
 
 function welcomeMessage(object) {
+var greeting = "Welcome ";
 
+    greeting += capitalizeWord(object.name) + '!';
+
+return greeting;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 8 - Profile Info /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// Should take an object with a name an a species and return '<Name> is a <Species>'
+// return object.name concat with ' is a ' + Species
 
 function profileInfo(object) {
-
+return capitalizeWord(object.name) + ' is a ' + capitalizeWord(object.species);
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 9 - Maybe Noises /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// check if this object has a noises array return them as a string separated by a space
+//if there are no noises return 'there are no noises'
 
 function maybeNoises(object) {
-
+if (Array.isArray(object.noises) && object.noises.length > 0){
+    return object.noises.join(' ');
+}
+return 'there are no noises';
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 10 - Has Words ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//Should take a string of words and a word and return true if <word> is in <string of words>, otherwise return falseShould take a string of words and a word and return true if <word> is in <string of words>,
+//otherwise return falseShould take a string of words and a word and return true if <word> is in <string of words>,
+//otherwise return false
 
+// use string.includes() to check if the word is in the string
+// if word matches,return true, otherwise, return false.
 function hasWord(string, word) {
 
+ if (string.includes(word)){
+     return true;
+ }
+ return false;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 11 - Add Friend //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//Should take a name and an object and add the name using .push to the object's friends array
+//then return the object
 
 function addFriend (name, object) {
-
+object.friends.push(name);
+return object;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 12 - Is Friend ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// chheck if the friends array in the object includes the input name
+// return true if <name> is a friend of <object> and false otherwise
 function isFriend(name, object) {
-
+if (Array.isArray(object.friends) && object.friends.includes(name)){
+    return true;
+}
+return false;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 13 - Non-Friends /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// Should take a name and a list of people
+// return a list of all the names that <name> is not friends with 
+// create an array
+// for loop through the input array
+// check if the object in the array includes the input name and the name is not equal to the name inside the array
+// push the name into the notFriends array
+// return notFriends array
 
 function nonFriends(name, array) {
-
+    //create an array called nonFriends
+    var nonFriends = [];
+    for(var i = 0; i < array.length; i++){
+        if(!array[i].friends.includes(name) && array[i].name != name){
+            nonFriends.push(array[i].name);
+        }
+    }
+return nonFriends;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 14 - Update Object ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// Should update the property <key> on <object> with new <value>.
+//If <key> does not exist on <object> create it.
+// update or add key/value using bracket notation
+// return object
 
 function updateObject(object, key, value) {
+object[key] = value;
+return object;
 
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 15 - Remove Properties ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// Should take an object and an array of strings.
+//Should remove any properties on <object> that are listed in <array> 
+// for in loop through thhe object
+// check if the object key is included in the array
+// if so delete object key property
 
 function removeProperties(object, array) {
-
+for (var key in object){
+    if (array.includes(key)){
+        delete object[key];
+    }
+}
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 16 - Dedup ///////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+// Should take an array and return an array with all the duplicates removed 
+// loop through the array
+// create a new array
+// check if index values match using index of, if so push values into the new array
+// return the array
 
 function dedup(array) {
+var unique = [];
 
+    for(var i = 0; i < array.length; i++){
+            if(unique.indexOf(array[i]) === -1) {
+                unique.push(array[i]);
+            }
+        }
+return unique;
 }
 
 //////////////////////////////////////////////////////////////////////
