6bd9412f4528063295b8013f26ac3946665cf1f3
Ubuntu <ubuntu@ip-172-31-32-165.ec2.internal>
2020-04-28 23:17:56 +0000

    lowdown and lets get functional updated

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -20,16 +20,24 @@ var _ = require("lodown-jazsmith24");
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
+// create a function that filters male customers from an array of customer objects
+// return the filter function called on the input array
+// the call back function should return customerobj that has a male gender
+// use the .lengthprop to return length of array
 
 var maleCount = function(array) {
     return _.filter(array, function(customerObj){
-        // filter male customers
+        // return the 
         return customerObj.gender === 'male';
     }).length;
 };
 
 
-// 
+//return  use the ude of reduce function to get the number of female cstomers in an array
+// check if the customerObj is a female 
+// add 1 to the seed
+// return seed
+
 var femaleCount = function(array){
    return _.reduce(array, function(prev, customerObj){
         if (customerObj.gender === 'female'){
@@ -52,6 +60,14 @@ let ageOfCustomers = 0;
     } return filterOutput;
 };
 
+// create a function that takes an array and returns the youngest customer
+// create a filterOutput variable assigned with an empty string
+// declare a variable called ageOfCustomers and assign it with Infinity
+// loop through tthe array
+// check if the customer age is greater that ageOfCustomers var
+// if so reassign ageOfCustomer to the current customer age
+// and reassign filterOutput ot the youngest customers age
+// return filter output
 var youngestCustomer = function(array) {
     let filterOutput = '';
     let ageOfCustomers = Infinity;
@@ -69,48 +85,38 @@ var youngestCustomer = function(array) {
 
 
 
-// var averageBalance = function(array){
-  
-// for (let i = 0; i < array.length; i++){
-  
-//   array[i].balance = array[i].balance.replace(/[^0-9.]/g, "");
-//   array[i].balance = parseFloat(array[i].balance);
-//   array[i].balance = Math.floor(array[i].balance);
-// }
-
-    
-// let bal = array.reduce(function(acc, personObj){
-//     return acc + personObj.balance;
-// }, 0);
-
-//  return bal / array.length;
-// };
+//create variable var sumOfAllBallances and assign it the value 0;    
+//loop array using for loop
+// assign oject balance to variable balanceStroing        
+// use slice to remove $        
+// use split to create array seperating element atcomma        
+// use join to join array        
+// convert to number datatype        
+// add and assign the value of balanceTypeOfNumber to sumOfAllBalances       
+//assign the length of array to numberOfCustomersWithBalance
+//return sumOfAllBalances / numberOfCustomersWithABalance
 
 var averageBalance = function(array){
-   
 var sumOfAllBalances = 0;
 var customersWithABalanceArray = [];
 
 for(var i = 0; i < array.length; i++){
-        customersWithABalanceArray.push(array[i].balance)
       
+        customersWithABalanceArray.push(array[i].balance)//use to get number to divid by
         let balanceString = array[i].balance;
-      
         let balanceStringNo$ = balanceString.slice(1);
-
         let balanceStringToArraySlitAtComma = balanceStringNo$.split(',');
-       
         let newBalanceString = balanceStringToArraySlitAtComma.join('');
-    
         let balanceTypeOfNumber = Number(newBalanceString);
-    
         sumOfAllBalances += balanceTypeOfNumber;
+       
 }
-
 var numberOfCustomersWithABalance = customersWithABalanceArray.length;
 
 return sumOfAllBalances / numberOfCustomersWithABalance;
 };
+
+
 
 
 
@@ -128,9 +134,13 @@ var firstLetterCount = function(array, letter){
 // Find how many friends of a given customer have names that start with a given letter
 //  - **Input**: `Array`, `Customer`, `Letter`
 //  - **Output**: `Number`
-// use map function to return 
-// loop through the given customer friends list
-// 
+// declare two variables, one called friends and one called frens
+// use _.each to iterate through the array to access customer objects in array
+// check if the customer given is equal to the customer name
+//if so, assign customer.friends array to the friends variable
+// assign filter called on friends array to the frens var
+// use filter function to return an array of friends who has the given letter at the beginning of the name
+// return frens.length
 
 var friendFirstLetterCount = function(array, customer, letter){
 
@@ -176,17 +186,20 @@ var friendsCount = function(array, name){
     return arr;
 };
 
+
+
 var topThreeTags = function(array){
-  
-var top3 = [];
+  // create an array
+var top3 = []; 
 
-_.each(array, function(element, index, collection) {
+_.each(array, function(element, index, collection) { // loop through the array
 
-_.each(element.tags, function(tag, index, collection) {
-    top3.push(tag);
+_.each(element.tags, function(tag, index, collection) {// loop through the tags array in object
+    top3.push(tag); // push tags into top3 array
 });
 });
 
+// reduce the array to the top three occurences within an object
 var reduced = _.reduce(top3,function(accObj, tags){
   
   if(accObj[tags]){
@@ -196,15 +209,21 @@ return accObj;
   accObj[tags] = 1;
   return accObj;
 },{});
+
+// create an empty array
 var sortedArray = [];
 
+// for-in loop throught the reduced object
+// push an object that includes reduced array tag assign with key and occured assign with the key values
 for (var key in reduced){
   sortedArray.push({tag: key, occured: reduced[key]});
 }
 
+// sort the array in order of greatest to least occurences
 sortedArray.sort(function (a,b){
   return b.occured - a.occured;
 });
+// return the top three occuring tags
 return [sortedArray[0].tag, sortedArray[1].tag, sortedArray[2].tag];
 };
 
@@ -218,10 +237,14 @@ return [sortedArray[0].tag, sortedArray[1].tag, sortedArray[2].tag];
 //  - **Input**: `Array`
 //  - **Output**: `Object`
 //  - **Constraints**: Use `reduce`
+
 var genderCount = function (array, classifier){
-    classifier = classifier || String;
-    return array.reduce(function (counter, item) {
+    classifier = classifier || String; // assign classifier to classifier var
+    
+    return array.reduce(function (counter, item) { // return function reduce called on array 
+    // declare var p assigned with classifier called on item gender
         var p = classifier(item.gender);
+    // assign counter[p] + 1 or 1 if the counter has own property
         counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
         return counter;
     }, {});
